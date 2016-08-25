dd45b03dc280db28fba3094640f77acfb8cba35f
Moritz Drexl <mdrexl@fastmail.fm>
2016-08-25 16:19:34 +0200

    start recursive cell view

--- client/src/Views/Cell.hs
+++ client/src/Views/Cell.hs
@@ -1,13 +1,72 @@
 module Views.Cell where
 
+import Data.Maybe
+import Data.Monoid
+import Data.Text (Text)
+
 import React.Flux
 
+import Lib.Model.Column
 import Lib.Model.Cell
+import Lib.Model.Types
 import Lib.Types
 
-cell_ :: CellContent -> ReactElementM eh ()
-cell_ !c = view cell c mempty
+import Store
 
-cell :: ReactView CellContent
-cell = defineView "cell" $ \c -> do
-  "cell"
+type CellCallback a = a -> StatefulViewEventHandler (Maybe Value)
+type CellEventHandler = StatefulViewEventHandler (Maybe Value)
+
+data CellProps = CellProps
+  { cpColId :: !(Id Column)
+  , cpRecId :: !(Id Record)
+  , cpColumn :: !Column
+  , cpContent :: !CellContent
+  }
+
+cell_ :: CellProps -> ReactElementM eh ()
+cell_ !props = view cell props mempty
+
+cell :: ReactView CellProps
+cell = defineStatefulView "cell" Nothing $ \mTmpVal props ->
+  case cpContent props of
+    CellError msg -> elemText $ "Error: " <> msg
+    CellValue val -> do
+      let col = cpColumn props
+          c = cpColId props
+          r = cpRecId props
+      value_ (columnInputType col)
+             (columnDataType col)
+             (fromMaybe val mTmpVal)
+             (\v _ -> ([], Just $ Just v))
+      button_
+        [ onClick $ \_ _ -> \case
+            Nothing -> ([], Nothing)
+            Just tmpVal -> (dispatch $ CellSetValue c r tmpVal, Nothing)
+        ] $ "Set"
+  where
+
+    value_ :: InputType -> DataType -> Value -> CellCallback Value
+           -> ReactElementM CellEventHandler ()
+    value_ !inpType !datType !val !cb =
+      case datType of
+        DataBool ->
+          let (VBool b) = val
+          in cellBool_ inpType b (cb . VBool)
+        DataString ->
+          let (VString s) = val
+          in cellString_ inpType s (cb . VString)
+
+    cellBool_ :: InputType -> Bool -> CellCallback Bool -> ReactElementM CellEventHandler ()
+    cellBool_ !inpType !b !cb =
+      case inpType of
+        ColumnInput -> undefined
+
+    cellString_ :: InputType -> Text -> CellCallback Text -> ReactElementM CellEventHandler ()
+    cellString_ !inpType !s !cb =
+      case inpType of
+        ColumnInput ->
+          input_
+            [ "value" &= s
+            , onChange $ \evt -> cb (target evt "value")
+            ]
+        ColumnDerived -> elemText s
--- client/src/Views/Table.hs
+++ client/src/Views/Table.hs
@@ -32,11 +32,12 @@ tableGrid = defineView "tableGrid" $ \st -> do
 
         getRecord y = let Just r = Map.lookup y recByIndex in uncurry Entity r
         getColumn x = let Just c = Map.lookup x colByIndex in uncurry Entity c
-        getCell x y = let Just cell' = do
-                            (c, _) <- Map.lookup x colByIndex
-                            (r, _) <- Map.lookup y recByIndex
-                            Map.lookup (Coords c r) cells
-                      in cell'
+        getCellProps x y = let Just res = do
+                                 (c, col) <- Map.lookup x colByIndex
+                                 (r, _)   <- Map.lookup y recByIndex
+                                 content  <- Map.lookup (Coords c r) cells
+                                 pure $ CellProps c r col content
+                           in res
 
         renderer (GridRenderArgs x y _)
           | x == 0 && y == (numRecs + 1) =
@@ -52,7 +53,7 @@ tableGrid = defineView "tableGrid" $ \st -> do
           | y == 0 && 0 < x && x <= numCols =
               column_ $ getColumn (x - 1)
           | 0 < x && x <= numCols && 0 < y && y <= numRecs =
-              cell_ $ getCell (x - 1) (y - 1)
+              cell_ $ getCellProps (x - 1) (y - 1)
           | otherwise = div_ mempty
 
         props = GridProps
