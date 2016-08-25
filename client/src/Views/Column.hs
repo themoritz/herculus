05f17ab1fb54f29c70c861ab6f7c9f6e91509c8f
Ruben Moor <ruben.moor@gmail.com>
2016-08-25 15:25:35 +0200

    ui: rename and remove columns

--- client/src/Views/Column.hs
+++ client/src/Views/Column.hs
@@ -2,6 +2,7 @@ module Views.Column where
 
 import React.Flux
 
+import Data.Maybe (fromMaybe)
 import           Data.Text (Text)
 import qualified Data.Text as Text
 
@@ -15,16 +16,17 @@ column_ :: Entity Column -> ReactElementM eh ()
 column_ !c = view column c mempty
 
 column :: ReactView (Entity Column)
-column = defineStatefulView "column" "" $ \curText (Entity i col) -> do
+column = defineStatefulView "column" Nothing $ \curText (Entity i col) -> do
   input_
     [ "placeholder" &= ("column name" :: Text)
-    , "value" &= columnName col
-    , onChange $ \evt _ -> ([], Just $ target evt "value")
+    , "value" &= fromMaybe (columnName col) curText
+    , onChange $ \evt _ -> ([], Just $ Just $ target evt "value")
     , onKeyDown $ \_ evt curState ->
-        if keyCode evt == 13 && not (Text.null curState) -- 13 = Enter
-           then (dispatch $ ColumnRename i curState, Just curState)
-           else ([], Nothing)
+        let txt = fromMaybe "" curState
+        in  if keyCode evt == 13 && not (Text.null txt) -- 13 = Enter
+              then (dispatch $ ColumnRename i txt, Just curState)
+              else ([], Nothing)
     ]
---   button_
---     [ onClick $ \_ _ -> (dispatch $ TableDeleteColumn i, Nothing)
---     ] $ "-"
+  button_
+    [ onClick $ \_ _ _ -> (dispatch $ TableDeleteColumn i, Nothing)
+    ] $ "-"
