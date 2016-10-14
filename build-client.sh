667289059264c95099bd6714bd41314e92791941
Ruben Moor <ruben.moor@gmail.com>
2016-10-14 14:40:59 +0200

    more verbose output for build lib in build-client

--- build-client.sh
+++ build-client.sh
@@ -6,7 +6,9 @@ JSDIR=assets/public/js
 mkdir -p $JSDIR
 
 # Client
-cd client && stack build client
+cd client && \
+    stack build lib && \
+    stack build client
 
 cp `stack path --local-install-root`/bin/client.jsexe/all.js ../$JSDIR/ghcjs.js
 
