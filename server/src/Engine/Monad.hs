b257971dc904a3ae780cc6572910da87a32175cd
Moritz Drexl <mdrexl@fastmail.fm>
2016-12-21 20:02:22 +0100

    implement first half of MonadEngine EngineT

--- server/src/Engine/Monad.hs
+++ server/src/Engine/Monad.hs
@@ -12,6 +12,7 @@ import           Control.Monad.Except
 import           Control.Monad.State
 
 import           Data.Foldable                (for_)
+import           Data.Functor                 (($>))
 import           Data.Map                     (Map)
 import qualified Data.Map                     as Map
 import           Data.Proxy
@@ -50,9 +51,11 @@ type What a = Lens' Changes (Map (Id a) (ChangeOp a))
 emptyChanges :: Changes
 emptyChanges = Changes Map.empty Map.empty Map.empty Map.empty
 
+-- | Note: cache does not have a notion that columns of cells have been deleted
+-- earlier in the transaction.
 data Cache = Cache
   { _cacheCells       :: Map (Id Column, Id Row) (Entity Cell)
-  , _cacheColumnCells :: Map (Id Column) [CellContent]
+  , _cacheColumnCells :: Map (Id Column) [Entity Cell]
   , _cacheColumns     :: Map (Id Column) Column
   }
 
@@ -160,14 +163,34 @@ instance MonadError AppError m => MonadError AppError (EngineT m) where
   catchError a h = EngineT $ unEngineT a `catchError` (unEngineT . h)
 
 instance MonadHexl m => MonadEngine (EngineT m) where
+
   graphGets f = f <$> use engineGraph
 
-  getColumnOfTableByName tableId name = do
-    let colQuery =
-          [ "name" =: name
-          , "tableId" =: toObjectId tableId
-          ]
-    eitherToMaybe <$> lift (getOneByQuery colQuery)
+  graphModify f = engineGraph %= f
+
+  graphSetCodeDependencies columnId deps = do
+    let getTableId c = _columnTableId <$> lift (getById' c)
+    graph <- use engineGraph
+    setCodeDependencies getTableId columnId deps graph >>= \case
+      Nothing -> pure True
+      Just graph' -> (engineGraph .= graph') $> False
+
+  graphSetTypeDependencies columnId deps = do
+    graph <- use engineGraph
+    case setTypeDependencies columnId deps graph of
+      Nothing     -> pure True
+      Just graph' -> (engineGraph .= graph') $> False
+
+  --
+
+  getTableByName name =
+    let query = [ "name" =: name ]
+    in  eitherToMaybe <$> lift (getOneByQuery query)
+
+  getColumnOfTableByName tableId name =
+    let query = [ "name" =: name
+                , "tableId" =: toObjectId tableId ]
+    in  eitherToMaybe <$> lift (getOneByQuery query)
 
   makeDefaultValue = \case
     DataBool     -> pure $ VBool False
@@ -182,17 +205,80 @@ instance MonadHexl m => MonadEngine (EngineT m) where
     DataList _   -> pure $ VList []
     DataMaybe _  -> pure $ VMaybe Nothing
 
+  --
+
+  createTable = logCreate changesTables
+
+  modifyTable tableId f = do
+    table <- lift $ getById' tableId
+    logUpdate changesTables tableId $ f table
+
   deleteTable tableId = do
     let query = [ "tableId" =: toObjectId tableId ]
     lift $ deleteByQuery (Proxy :: Proxy Column) query
     lift $ deleteByQuery (Proxy :: Proxy Cell) query
     lift $ deleteByQuery (Proxy :: Proxy Row) query
-    opDelete changesTables tableId
+    logDelete changesTables tableId
+
+  createColumn column = do
+    columnId <- logCreate changesColumns column
+    engineCache . cacheColumns . at columnId .= Just column
+    pure columnId
+
+  modifyColumn columnId f = do
+    column <- getColumn columnId
+    let newColumn = f column
+    logUpdate changesColumns columnId newColumn
+    engineCache . cacheColumns . at columnId .= Just column
 
   deleteColumn columnId = do
+    -- Cascade delete cells
     lift $ deleteByQuery (Proxy :: Proxy Cell)
       [ "columnId" =: toObjectId columnId ]
-    opDelete changesColumns columnId
+    logDelete changesColumns columnId
+
+  createRow row = do
+    -- FIXME: We invalidate the columnCells cache. This should work since the
+    -- next time a column is queried, the cells will have been created.
+    -- Still really ugly.
+    columns <- getTableColumns $ row ^. rowTableId
+    for_ columns $ \(Entity columnId _) ->
+      engineCache . cacheColumnCells . at columnId .= Nothing
+    logCreate changesRows row
+
+  deleteRow rowId = do
+    -- Cascade delete cells
+    lift $ deleteByQuery (Proxy :: Proxy Cell)
+      [ "rowId" =: toObjectId rowId ]
+    -- Update cache
+    row <- getRow rowId
+    columns <- getTableColumns $ row ^. rowTableId
+    for_ columns $ \(Entity columnId _) -> do
+      engineCache . cacheCells . at (columnId, rowId) .= Nothing
+      engineCache . cacheColumnCells . at columnId . _Just %= filter
+        (\(Entity _ cell) -> cell ^. cellRowId /= rowId)
+    -- Log
+    logDelete changesRows rowId
+
+  createCell cell = do
+    cellId <- logCreate changesCells cell
+    engineCache . cacheCells . at (cell ^. cellColumnId, cell ^. cellRowId) .=
+      Just (Entity cellId cell)
+    pure cellId
+
+  setCellContent columnId rowId content = do
+    Entity cellId cell <- getCell columnId rowId
+    let cell' = cell & cellContent .~ content
+    engineCache . cacheCells . at (columnId, rowId) .=
+      Just (Entity cellId cell')
+    -- FIXME: Need also to update the column lists, but should be safe for now.
+    logUpdate changesCells cellId cell'
+
+  --
+
+  -- TODO: cache functions
+
+  --
 
   scheduleCompileColumn columnId = do
     engineCompileTargets . at columnId .= Just ()
@@ -205,6 +291,8 @@ instance MonadHexl m => MonadEngine (EngineT m) where
     engineEvalTargets . at columnId . non (SpecificRows Set.empty) %= \case
       SpecificRows rows -> SpecificRows $ Set.insert rowId rows
       CompleteColumn    -> CompleteColumn
+
+  getCompileTargets = Set.toList <$> use engineCompileTargets
 
   getEvalRoots = Map.keys <$> use engineEvalTargets
 
@@ -222,7 +310,16 @@ eitherToMaybe :: Either a b -> Maybe b
 eitherToMaybe (Left _)  = Nothing
 eitherToMaybe (Right b) = Just b
 
-opDelete :: (MonadHexl m, Model a) => What a -> Id a -> EngineT m ()
-opDelete what i = do
-  engineChanges . what . at i .= Just Delete
-  lift $ delete i
+-- | Also actually creates the entity, because otherwise we wouldn't be able
+-- to get the id...
+logCreate :: (MonadHexl m, Model a) => What a -> a -> EngineT m (Id a)
+logCreate what a = do
+  i <- lift $ create a
+  engineChanges . what . at i .= Just (Create a)
+  pure i
+
+logUpdate :: MonadHexl m => What a -> Id a -> a -> EngineT m ()
+logUpdate what i a = engineChanges . what . at i .= Just (Update a)
+
+logDelete :: MonadHexl m => What a -> Id a -> EngineT m ()
+logDelete what i = engineChanges . what . at i .= Just Delete
