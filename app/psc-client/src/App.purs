b824f6adeafe1f8e5e11a01766799743906af840
Moritz Drexl <mdrexl@fastmail.fm>
2017-02-07 18:11:03 +0100

    tie the error bubbling knot

--- app/psc-client/src/App.purs
+++ app/psc-client/src/App.purs
@@ -3,78 +3,87 @@ module App where
 import Prelude
 import Halogen as H
 import Halogen.HTML as HH
-import Lib.Api.Rest as Api
-import Control.Monad.Aff.Class (liftAff)
-import Control.Monad.Aff.Console (log)
+import Play as Play
+import Control.Coroutine (emit)
+import Control.Monad.Aff (Aff)
+import Control.Monad.Aff.AVar (AVAR, AVar, makeVar, putVar, takeVar)
 import Control.Monad.Except (ExceptT, runExceptT)
-import Control.Monad.State (modify)
-import Data.Array (head, length)
-import Data.Either (Either(Right, Left))
-import Data.Lens ((^.))
-import Data.Maybe (Maybe(Nothing, Just))
-import Lib.Api.Schema.Auth (LoginData(..), LoginResponse(..), uiSessionKey, uiUserName)
-import Lib.Api.Schema.Project (Command(..), Project(..), ProjectData(..))
-import Lib.Model (Entity(..))
-import Lib.Model.Auth (Email(..))
-import Lib.Model.Table (Table(..))
-import Lib.Custom (Id(..))
+import Control.Monad.Reader (ask)
+import Control.Monad.Trans.Class (lift)
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
+import Halogen (liftAff)
+import Halogen.Query.EventSource (EventSource(..), SubscribeStatus(..))
 import Servant.PureScript.Affjax (AjaxError, errorToString)
 import Types (AppM)
 
-type State = Unit
+data Query a
+  = SetError String a
+  | Initialize a
 
-data Query a = Init a
+type State =
+  { error :: String
+  , var   :: Maybe (AVar String)
+  }
 
 app :: forall i o eff. H.Component HH.HTML Query i o (AppM eff)
-app = H.lifecycleComponent
-    { initialState: const unit
-    , receiver: const Nothing
-    , render
-    , eval
-    , initializer: Just $ H.action Init
-    , finalizer: Nothing
-    }
+app = H.lifecycleParentComponent
+  { initialState: const { error: "", var: Nothing }
+  , render
+  , eval
+  , receiver: const Nothing
+  , initializer: Just (H.action Initialize)
+  , finalizer: Nothing
+  }
+
   where
 
-    render :: State -> H.ComponentHTML Query
+    render :: State -> H.ParentHTML Query Play.Query Unit (AppM eff)
     render st = HH.div_
-        [
-        ]
-    
-    eval :: Query ~> H.ComponentDSL State Query o (AppM eff)
-    eval (Init next) = do
-      let loginData = LoginData
-            { ldEmail: Email { unEmail: "mdrexl@fastmail.fm" }
-            , ldPassword: "Andreas"
-            }
+      [ renderPlay
+      , HH.text st.error
+      ]
+      where renderPlay = case st.var of
+              Nothing -> HH.text "loading"
+              Just var -> HH.slot unit (Play.play (env var)) unit (const Nothing)
+      
 
-      apiCall (Api.postAuthLogin loginData) $ case _ of
-        LoginFailed e -> liftAff $ log e
-        LoginSuccess userInfo -> do
-          liftAff $ log $ userInfo ^. uiUserName
-          let auth = userInfo ^. uiSessionKey
-          apiCall (Api.getProjectList auth) $ \ps -> case head ps of
-            Nothing -> pure unit
-            Just (Project p) -> do
-              liftAff $ log $ p._projectName
-              apiCall (Api.getProjectLoadByProjectId auth p._projectId) $ \(ProjectData pd) ->
-                case head pd._pdTables of
-                  Nothing -> pure unit
-                  Just (Entity e) -> do
-                    liftAff $ log "Done."
-
+    eval :: Query ~> H.ParentDSL State Query Play.Query Unit o (AppM eff)
+    eval (SetError e next) = do
+      H.modify _{ error = e }
       pure next
+
+    eval (Initialize next) = do
+      var <- liftAff makeVar
+      H.modify _{ var = Just var }
+      H.subscribe $ EventSource $ do
+        let go = do
+              e <- lift $ liftAff $ takeVar var
+              emit (SetError e Listening)
+              go
+        pure { producer: go, done: pure unit }
+      pure next
+
 
 -- Utils -----------------------------------------------------------------------
 
+env :: AVar String -> Play.Env
+env var =
+  { runApi: apiCall (putVar var)
+  }
+
 -- | Logs `AjaxError`s to the console.
 apiCall
-  :: forall eff s i o a
-   . ExceptT AjaxError (AppM eff) a
-  -> (a -> H.ComponentDSL s i o (AppM eff) Unit)
+  :: forall eff s i o
+   . (forall eff'. String -> Aff (avar :: AVAR | eff') Unit)
+  -> ExceptT AjaxError (H.ComponentDSL s i o (AppM eff)) Unit
   -> H.ComponentDSL s i o (AppM eff) Unit
-apiCall call handler = do
-  result <- H.lift $ runExceptT call
+apiCall throw action = do
+  params <- H.lift ask
+  result <- runExceptT action
   case result of
-    Left err -> liftAff $ log $ errorToString err
-    Right a -> handler a
+    Left err -> do
+      let msg = errorToString err
+      liftAff $ throw msg
+    Right a -> do
+      pure a
