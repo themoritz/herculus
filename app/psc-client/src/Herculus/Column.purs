34cadc0760b519d83b0f0629dce9d8d43c788991
Moritz Drexl <mdrexl@fastmail.fm>
2017-02-17 12:34:47 +0100

    editbox

--- app/psc-client/src/Herculus/Column.purs
+++ app/psc-client/src/Herculus/Column.purs
@@ -5,15 +5,18 @@ import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
+import Herculus.EditBox as Edit
 import Data.Map (Map)
 import Herculus.Monad (Herc)
-import Lib.Api.Schema.Column (Column(..))
+import Herculus.Utils (cldiv_)
+import Lib.Api.Schema.Column (Column(..), columnName)
 import Lib.Custom (Id(..))
 import Lib.Model.Column (DataType, IsDerived, ReportFormat, ReportLanguage)
 import Lib.Model.Table (Table(..))
 
 data Query a
   = Update Input a
+  | SetName' String a
 
 type Input =
   { column :: Column
@@ -31,27 +34,42 @@ type State =
   }
 
 type Child =
-  Const Void
+  Edit.Query String
 
 type Slot =
-  Void
+  Unit
 
 comp :: H.Component HH.HTML Query Input Output Herc
 comp = H.parentComponent
   { initialState:
     { input: _
     }
-  , receiver: const Nothing
+  , receiver: Just <<< H.action <<< Update
   , render
   , eval
   }
 
 render :: State -> H.ParentHTML Query Child Slot Herc
-render st = HH.text "Column"
+render st = cldiv_ "column"
+  [ cldiv_ "column__head"
+    [ HH.slot unit Edit.comp
+              { value: st.input.column ^. columnName
+              , placeholder: "Name..."
+              , className: "editbox--column-name"
+              , show: id
+              , validate: Just
+              }
+              (Just <<< H.action <<< SetName')
+    ]
+  ]
 
 eval :: Query ~> H.ParentDSL State Query Child Slot Output Herc
 eval = case _ of
 
   Update input next -> do
     modify _{ input = input }
+    pure next
+
+  SetName' name next -> do
+    H.raise $ SetName name
     pure next
---
+++ app/psc-client/src/Herculus/EditBox.purs
@@ -0,0 +1,124 @@
+module Herculus.EditBox where
+
+import Herculus.Prelude
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+import DOM.Event.KeyboardEvent (code)
+import Herculus.Monad (Herc)
+import Herculus.Utils (cldiv)
+
+data Query v a
+  = Update (Input v) a
+  | StartEdit a
+  | SetText String a
+  | TrySave a
+  | CancelEdit a
+
+type Input v =
+  { value :: v
+  , placeholder :: String
+  , className :: String
+  , show :: v -> String
+  , validate :: String -> Maybe v
+  }
+
+type State v =
+  { input :: Input v
+  , tmpValue :: Maybe v
+  , invalidText :: Maybe String
+  , editing :: Boolean
+  }
+
+comp :: forall v. H.Component HH.HTML (Query v) (Input v) v Herc
+comp = H.component
+  { initialState:
+    { input: _
+    , tmpValue: Nothing
+    , invalidText: Nothing
+    , editing: false
+    }
+  , render
+  , eval
+  , receiver: Just <<< H.action <<< Update
+  }
+
+render :: forall v. State v -> H.ComponentHTML (Query v)
+render st = HH.div
+  [ HP.classes
+    [ H.ClassName "editbox"
+    , H.ClassName st.input.className
+    ]
+  ]
+  [ case st.editing of
+      false -> cldiv (if text == "" then "editbox__placeholder" else "")
+        [ HE.onClick (HE.input_ StartEdit)
+        ]
+        [ HH.text (if text == "" then st.input.placeholder else text) ]
+      true -> HH.input
+        [ HP.placeholder st.input.placeholder
+        , HP.classes
+          [ H.ClassName "editbox__input"
+          , H.ClassName (if isJust st.invalidText then "editbox__input--invalid" else "")
+          ]
+        , HP.value text
+        , HP.autofocus true
+        , HE.onValueInput (HE.input SetText)
+        , HE.onKeyDown \e -> case code e of
+            "Enter"  -> Just (H.action TrySave)
+            "Escape" -> Just (H.action CancelEdit)
+            _        -> Nothing
+        -- TODO: onBlur
+        ]
+   ]
+
+   where
+
+   text = fromMaybe (st.input.show st.input.value) $
+          st.input.show <$> st.tmpValue
+      <|> st.invalidText
+
+eval :: forall v. Query v ~> H.ComponentDSL (State v) (Query v) v Herc
+eval = case _ of
+
+  Update input next -> do
+    modify _{ input = input }
+    pure next
+
+  StartEdit next -> do
+    modify _{ editing = true }
+    pure next
+
+  SetText str next -> do
+    validate <- gets _.input.validate
+    case validate str of
+      Nothing -> modify _
+        { tmpValue = Nothing
+        , invalidText = Just str
+        }
+      Just v -> modify _
+        { tmpValue = Just v
+        , invalidText = Nothing
+        }
+    pure next
+    
+  TrySave next -> do
+    gets _.tmpValue >>= case _ of
+      Nothing -> reset
+      Just v -> do
+        reset
+        H.raise v
+    pure next
+
+  CancelEdit next -> do
+    reset
+    pure next
+
+  where
+
+  reset = modify _
+    { tmpValue = Nothing
+    , invalidText = Nothing
+    , editing = false
+    }
